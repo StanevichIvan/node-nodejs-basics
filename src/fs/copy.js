c0d7a052ab540c538ab13b80e6942af911228345
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-25 23:14:50 +0100

    feature(task-1): mplement function that copies folder files files with all its content into folder files_copy at the same level (if files folder doesn't exists or files_copy has already been created Error with message FS operation failed must be thrown)

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,19 @@
+import { cp } from "node:fs/promises";
+
+const SOURCE_DIR = "./files";
+const DEST_DIR = "./files_copy";
+const ERROR_MESSAGE = "FS operation failed";
+
 const copy = async () => {
-    // Write your code here 
+  try {
+    await cp(SOURCE_DIR, DEST_DIR, {
+      recursive: true,
+      errorOnExist: true,
+      force: false,
+    });
+  } catch (e) {
+    throw ERROR_MESSAGE;
+  }
 };
 
 await copy();
