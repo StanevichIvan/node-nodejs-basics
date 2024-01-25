001609d090b7a712459d9117ed714708cfeed84c
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-25 22:48:50 +0100

    feature(task-1): implement function that creates new file fresh.txt with content I am fresh and young inside of the files folder (if file already exists Error with message FS operation failed must be thrown)

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,23 @@
+import { open } from "node:fs/promises";
+
+const FILE_NAME = "fresh.txt";
+const PATH = "./files";
+const ERROR_MESSAGE = "FS operation failed";
+const FILE_CONTENT = "I am fresh and young";
+
 const create = async () => {
-    // Write your code here 
+  let file = null;
+
+  try {
+    file = await open(`${PATH}/${FILE_NAME}`, "wx");
+    file.write(FILE_CONTENT);
+  } catch (e) {
+    if (e.code === "EEXIST") {
+      throw ERROR_MESSAGE;
+    }
+  } finally {
+    file?.close();
+  }
 };
 
 await create();
