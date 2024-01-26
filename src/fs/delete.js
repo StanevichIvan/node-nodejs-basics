312a7c9a504b74a67f2a98956bdd6cb2e0d1dd40
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-26 12:03:24 +0100

    implement function that deletes file fileToRemove.txt (if there's no file fileToRemove.txt Error with message FS operation failed must be thrown)

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,5 +1,15 @@
+import { rm } from "node:fs/promises";
+
+const SOURCE_DIR = "./files";
+const FILE_NAME = "fileToRemove.txt";
+const ERROR_MESSAGE = "FS operation failed";
+
 const remove = async () => {
-    // Write your code here 
+  try {
+    await rm(`${SOURCE_DIR}/${FILE_NAME}`);
+  } catch (e) {
+    throw ERROR_MESSAGE;
+  }
 };
 
 await remove();
