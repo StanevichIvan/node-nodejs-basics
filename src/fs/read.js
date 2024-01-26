b805c50cd5091fe63780c5b7f70c73491a1e58d8
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-26 12:13:28 +0100

    implement function that prints content of the fileToRead.txt into console (if there's no file fileToRead.txt Error with message FS operation failed must be thrown)

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,18 @@
+import { readFile } from "node:fs/promises";
+
+const SOURCE_DIR = "./files";
+const FILE_NAME = "fileToRead.txt";
+const ERROR_MESSAGE = "FS operation failed";
+
 const read = async () => {
-    // Write your code here 
+  try {
+    const content = await readFile(`${SOURCE_DIR}/${FILE_NAME}`, {
+      encoding: "utf8",
+    });
+    console.log(content);
+  } catch (e) {
+    throw ERROR_MESSAGE;
+  }
 };
 
 await read();
