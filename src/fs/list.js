7cf0235135ba5754e213e20298d4181772701e1f
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-26 12:09:29 +0100

    implement function that prints all array of filenames from files folder into console (if files folder doesn't exists Error with message FS operation failed must be thrown)

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,15 @@
+import { readdir } from "node:fs/promises";
+
+const SOURCE_DIR = "./files";
+const ERROR_MESSAGE = "FS operation failed";
+
 const list = async () => {
-    // Write your code here 
+  try {
+    const files = await readdir(SOURCE_DIR);
+    for (const file of files) console.log(file);
+  } catch (e) {
+    throw ERROR_MESSAGE;
+  }
 };
 
 await list();
