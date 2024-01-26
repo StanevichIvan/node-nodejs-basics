c4df52222e34fe833e70d4b69d87a26b3a4e1c14
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-26 16:26:15 +0100

    mplement function that parses environment variables with prefix RSS_ and prints them to the console in the format RSS_name1=value1; RSS_name2=value2

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,5 +1,11 @@
+import { env } from "node:process";
+
+const EVN_VARIABLE_PREFIX = "RSS_";
+
 const parseEnv = () => {
-    // Write your code here 
+  for (const key of Object.keys(env)) {
+    console.log(`${EVN_VARIABLE_PREFIX}${key}=${env[key]}`);
+  }
 };
 
 parseEnv();
