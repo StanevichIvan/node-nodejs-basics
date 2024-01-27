8da585305a14b560b626e3daea2addbee74d5772
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-27 18:59:20 +0100

    implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream

--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,16 @@
+// implement function that writes process.stdin data into file fileToWrite.txt content using Writable Stream
+import { createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { stdin } from "node:process";
+
+const FILE_PATH = "./files/fileToWrite.txt";
+
 const write = async () => {
-    // Write your code here 
+  try {
+    await pipeline(stdin, createWriteStream(FILE_PATH));
+  } catch (e) {
+    throw e;
+  }
 };
 
 await write();
