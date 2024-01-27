b4f2818a0b66184298af4afe69b721a38b3777ce
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-27 18:47:35 +0100

    implement function that reads file fileToRead.txt content using Readable Stream

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,18 @@
+// implement function that reads file fileToRead.txt content using Readable Stream
+// and prints it's content into process.stdout
+import { createReadStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { stdout } from "node:process";
+
+const FILE_PATH = "./files/fileToRead.txt";
+
 const read = async () => {
-    // Write your code here 
+  // Write your code here
+  try {
+    await pipeline(createReadStream(FILE_PATH), stdout);
+  } catch (e) {
+    throw e;
+  }
 };
 
 await read();
