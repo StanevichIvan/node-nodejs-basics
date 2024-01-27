83aa40b3066aacd3d3e3cd334644c4645690abfe
Ivan Stanevich <ivan_stanevich@waters.com>
2024-01-27 19:28:56 +0100

    implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,21 @@
+// implement function that compresses file fileToCompress.txt to archive.gz using zlib and Streams API
+import { createReadStream, createWriteStream } from "node:fs";
+import { pipeline } from "node:stream/promises";
+import { createGzip } from "node:zlib";
+
+const FILE_PATH = "./files/fileToCompress.txt";
+const FILE_ARCHIVE_PATH = "./files/archive.gz";
+
 const compress = async () => {
-    // Write your code here 
+  try {
+    await pipeline(
+      createReadStream(FILE_PATH),
+      createGzip(),
+      createWriteStream(FILE_ARCHIVE_PATH)
+    );
+  } catch (e) {
+    throw e;
+  }
 };
 
 await compress();
